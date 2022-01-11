e404f13e27772bf7c2c7435908a2ace234dd4d87
shivamSinghkarix <87415108+shivamSinghkarix@users.noreply.github.com>
2022-01-11 13:08:36 +0530

    Initial commit

---
+++ src/main/java/shivam/demo/PipelineJenkinsTestApplication.java
@@ -0,0 +1,13 @@
+package shivam.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineJenkinsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelineJenkinsTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shivam/demo/PipelineJenkinsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package shivam.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineJenkinsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
